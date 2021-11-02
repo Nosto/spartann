027d124fce5ab7f31e464dd6a246f3d46c482e59
Mridang Agarwalla <mridang@nosto.com>
2021-11-02 09:37:34 +0200

    Updated the inspection script.

--- inspect.sh
+++ inspect.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --env GITHUB_WORKSPACE=/app --volume="$(pwd)"/.:/app docker.pkg.github.com/mridang/action-idea/idealize:latest /app /app/.idea/inspectionProfiles/CI.xml /out v2 Inspection "1347"
+docker run --env GITHUB_WORKSPACE=/app --volume="$(pwd)"/.:/app docker.pkg.github.com/mridang/action-idea/idealize:2020.3.3 /app /app/.idea/inspectionProfiles/CI.xml /out v2 Inspection noop 1347
