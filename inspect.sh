381226b535654ee0d4f600353076c273ea46b430
Mridang Agarwalla <mridang@nosto.com>
2022-05-19 11:16:07 +0300

    Improved the Gradle build

--- inspect.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-docker run --env GITHUB_WORKSPACE=/app --volume="$(pwd)"/.:/app docker.pkg.github.com/mridang/action-idea/idealize:2020.3.3 /app /app/.idea/inspectionProfiles/CI.xml /out v2 Inspection noop 1347
